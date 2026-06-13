7c8de29bcf8082aedc74a5f83ac47bd1dc2afc8a
yashu-0124 <yashaswini242324@gmail.com>
2026-06-13 11:35:43 +0530

    main.py

--- backened/main.py
+++ backened/main.py
@@ -1,1 +1,41 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
+from database import Base, engine, get_db
+from models import Request
+from schemas import RequestCreate
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="AI Disaster Volunteer Network")
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "AI Disaster Volunteer Network API Running"
+    }
+
+
+@app.post("/requests")
+def create_request(data: RequestCreate, db: Session = Depends(get_db)):
+
+    obj = Request(
+        name=data.name,
+        phone=data.phone,
+        request_type=data.request_type,
+        location=data.location,
+        priority="Pending",
+        status="Open"
+    )
+
+    db.add(obj)
+    db.commit()
+    db.refresh(obj)
+
+    return obj
+
+
+@app.get("/requests")
+def get_requests(db: Session = Depends(get_db)):
+    return db.query(Request).all()
