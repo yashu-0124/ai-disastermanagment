95cc93e6d3baabc3dbac3407954c3abb8cdfe49c
yashu-0124 <yashaswini242324@gmail.com>
2026-06-13 11:34:02 +0530

    model.py

--- backened/model.py
+++ backened/model.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
 
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    phone = Column(String)
+    request_type = Column(String)
+    location = Column(String)
+    priority = Column(String)
+    status = Column(String)
