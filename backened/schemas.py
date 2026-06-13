b55179e0125fad843f6ac5fa1ddce9fac73d2920
yashu-0124 <yashaswini242324@gmail.com>
2026-06-13 11:34:45 +0530

    schemas.py

--- backened/schemas.py
+++ backened/schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
 
+
+class RequestCreate(BaseModel):
+    name: str
+    phone: str
+    request_type: str
+    location: str
+
+
+class RequestResponse(RequestCreate):
+    id: int
+    priority: str
+    status: str
+
+    class Config:
+        from_attributes = True
